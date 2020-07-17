8f41c97ac8ffdf1ba63d6cbac15f8e4b3de2d5b8
dcribb19 <daniel.cribb.10@gmail.com>
2020-07-17 17:22:35 -0400

    Added code example.

--- travel_folium.py
+++ travel_folium.py
@@ -1,54 +1,10 @@
 # travel.py
-# Create an overlay on the U.S. map of states traveled to.
+# Create a personal overlay on the U.S. map of states traveled to.
 
 import folium
 import os
 
-"""
-TODO:
-- Add title to map. 
-- Change GeoJsons to not show all upon opening
-- See if file can be saved other than html
-- Remove people dicts
-- Create new way to enter name and states traveled to.
-"""
-
 state_geo = os.path.join(os.getcwd(), 'us-states.json')
-
-dc = {'Name' : 'DC',
-      'States' : ['VA', 'NC', 'SC', 'GA', 'FL', 'AL', 'WV', 'DC',
-                  'PA', 'MD', 'DE', 'NJ', 'NY', 'MA', 'NH', 'ME','LA', 'OH', 'IN', 'MI', 'IL', 'WI', 'IA', 'MN',
-                  'CO', 'NV', 'CA'
-                 ],
-      'Countries' : ['United States of America', 'Ireland',
-                     'Netherlands', 'Belgium', 'France',
-                     'Denmark', 'Spain', 'Switzerland', 'Germany','Costa Rica', 'Cayman Islands', 'Honduras','Belize', 'Mexico'
-                    ]
-     }
-
-j = {'Name' : 'Julia',
-     'States' : ['MA', 'NH', 'VT', 'RI', 'CT', 'NY', 'PA', 'NJ',
-                 'MD', 'DE', 'WV', 'VA', 'NC', 'SC', 'GA', 'FL','MS', 'LA', 'TX', 'MI', 'MN', 'IL', 'CA', 'HI',
-                 'DC'
-                ],
-     'Countries' : ['United States of America', 'Canada',
-                    'Mexico', 'Cayman Islands', 'Honduras','Belize', 'Aruba', 'Puerto Rico',
-                    'Costa Rica', 'Chile','Argentina', 'Spain','Portugal', 'Morocco', 'Italy', 'France'
-                   ]
-    }
-
-camille = {'Name' : 'Camille',
-           'States' : ['ME', 'VT', 'NH', 'RI', 'CT', 'NY', 'PA',
-                       'MD', 'WV', 'NC', 'FL', 'LA', 'TX', 'CA',
-                       'HI', 'DC', 'NJ', 'DE', 'MA', 'VA'
-                      ]
-          }
-
-debbie = {'Name' : 'Debbie',
-          'States' : ['TN', 'VA', 'OH', 'DC', 'NY', 'PA', 'NC',
-                      'SC', 'MD', 'WV', 'FL', 'GA'
-                     ]
-         }
 
 #initialize map
 us_map = folium.Map(location=[39.833333, -98.583333],
@@ -90,10 +46,16 @@ def states_traveled(person, color):
                    ).add_to(us_map)
 
 
-states_traveled(dc, 'green')
-states_traveled(j, 'blue')
-states_traveled(camille, 'purple')
-states_traveled(debbie, 'brown')
+'''
+EXAMPLE CODE:
+
+person_1 = {
+    'Name' : 'Person 1',
+    'States' : ['ME', 'FL', 'CA', 'WA', 'OR']
+}
+
+states_traveled(person_1, 'red')
+'''
 
 folium.LayerControl(hideSingleBase=True).add_to(us_map)
 us_map.save('us_map.html')
